2153ab61f1d1e2dfd86e7efdb4bfcccf640bdcc6
Eric Chang <echang25@hotmail.com>
2017-02-08 17:48:46 -0500

    gallery collection functionality complete

--- UPic/UploadViewController.swift
+++ UPic/UploadViewController.swift
@@ -156,31 +156,32 @@ class UploadViewController: UIViewController, UICollectionViewDelegate, UICollec
         print("From upload, users display name \(FIRAuth.auth()?.currentUser?.displayName)")
         let imageName = NSUUID().uuidString
         let user = FIRAuth.auth()?.currentUser
-       
+        
         if user?.uid != nil {
-       
+            
             let storageRef = FIRStorage.storage().reference().child("\(imageName).png")
-       
+            
             if let uploadData = UIImagePNGRepresentation(self.selectedImage!) {
-            storageRef.put(uploadData, metadata: nil, completion: { (metadata, error) in
-                if error != nil {
-                    print(error)
-                }
-               
-                let autoKey = FIRDatabase.database().reference().child("users").child((user?.uid)!).childByAutoId().key
-                
+                storageRef.put(uploadData, metadata: nil, completion: { (metadata, error) in
+                    if error != nil {
+                        print(error)
+                    }
+                    
+                    let autoKey = FIRDatabase.database().reference().child("users").child((user?.uid)!).childByAutoId().key
+                    
                     FIRDatabase.database().reference().child("categories").child(self.catogorySegmentedControl.titleForSegment(at: self.catogorySegmentedControl.selectedSegmentIndex)!).updateChildValues([self.titleTextField.text!: String(describing: metadata!.downloadURL()!)])
-
-                //(String(describing: metadata!.downloadURL()!))
-                FIRDatabase.database().reference().child("users").child((user?.uid)!).child("uploads").updateChildValues([self.titleTextField.text! : String(describing: metadata!.downloadURL()!)])
-//
-                metadata?.customMetadata = [
-                    "upvotes": "0",
-                    "downvotes": "0"
-                ]
-                print(metadata!.customMetadata)
-                
-            })
+                    
+                    //(String(describing: metadata!.downloadURL()!))
+                    FIRDatabase.database().reference().child("users").child((user?.uid)!).child("uploads").updateChildValues([self.titleTextField.text! : String(describing: metadata!.downloadURL()!)])
+                    //
+                    metadata?.customMetadata = [
+                        "upvotes": "0",
+                        "downvotes": "0"
+                    ]
+                    print(metadata!.customMetadata)
+                    
+                })
+            }
         }
         else {
             let alert = UIAlertController(title: "Error", message: "You need to log in to upload images", preferredStyle: .alert)
